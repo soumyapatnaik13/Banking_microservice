719f6ebed6aa766063304033edf373d2f350d529
Soumya Patnaik <141363016+soumyapatnaik13@users.noreply.github.com>
2026-04-09 15:12:07 +0530

    Add files via upload

---
+++ cards/src/main/java/com/soumya/cards/repository/CardsRepository.java
@@ -0,0 +1,16 @@
+package com.soumya.cards.repository;
+
+import com.soumya.cards.entity.Cards;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CardsRepository extends JpaRepository<Cards, Long> {
+
+    Optional<Cards> findByMobileNumber(String mobileNumber);
+
+    Optional<Cards> findByCardNumber(String cardNumber);
+
+}
