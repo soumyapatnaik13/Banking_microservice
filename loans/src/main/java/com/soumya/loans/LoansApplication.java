ba1c252fc760f2bd53b96c76a62dd9512a764cca
Soumya Patnaik <141363016+soumyapatnaik13@users.noreply.github.com>
2026-04-09 15:16:32 +0530

    Add files via upload

---
+++ loans/src/main/java/com/soumya/loans/LoansApplication.java
@@ -0,0 +1,45 @@
+package com.soumya.loans;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.soumya.loans.controller") })
+@EnableJpaRepositories("com.soumya.loans.repository")
+@EntityScan("com.soumya.loans.model")*/
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				
+				info = @Info(
+				title = "Accounts microservice REST API Documentation",
+				description = "EazyBank Accounts microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Soumya Patnaik",
+						email = "soumyapatnaik493@gmail.com",
+						
+				),
+				license = @License(
+						name = "Apache 2.0",
+					
+				)
+		),
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "EazyBank Loans microservice REST API Documentation",
+				url = "https://www.eazybytes.com/swagger-ui.html"
+		)
+)
+public class LoansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoansApplication.class, args);
+	}
+}
